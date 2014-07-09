9796c3bc316fbf5024d242659bfde4f3f0de1e0d
deepankar <deepankar@railsfactory.org>
2014-07-09 17:30:51 +0530

    with separate def fo different function

---
+++ todo/lib/marking.rb
@@ -0,0 +1,15 @@
+class Marked
+def self.mark(i)
+m = []
+
+m = open('todo.txt').map { |line| line.split('\n')[0] }
+m[i].gsub!("#undone","#done").strip
+f = File.open("todo.txt", "w")
+f.write(m)
+f.close
+f1 = File.open("todo.txt", "r")
+str = f1.read
+f1.close
+return str
+end
+end
--- todo/test/pending_test.rb
+++
@@ -1,8 +0,0 @@
-require '../lib/status.rb'
-require 'test/unit'
-class Teststorage < Test::Unit::TestCase
-def test_one      
-     assert_equal 'wakeup #undone bath #undone go to office #undone',Status.pend
-end
-end
-
