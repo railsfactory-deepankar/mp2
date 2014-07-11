e1540904187026f851c34eed79028c0fbaeb421b
deepankar <deepankar@railsfactory.org>
2014-07-11 14:26:52 +0530

    test cases using object

--- todo_final/lib/todo.rb
+++ todo_final/lib/todo.rb
@@ -24,12 +24,8 @@ return $completed.length
 end
 
 def add(items)
-#@todo = []
-#@pending = []
 $todo << items
 $pending << items
-#self.save
-#self.load1
 return $pending.length
 end
 
@@ -41,15 +37,11 @@ end
 
 def delete(num)
 $completed.delete_at(num-1)
-#self.save
-#self.load1
 return $completed.size
 end
 
 def modify(num , item)
 $pending[num-1] = item
-#self.save
-#self.load1
 return $pending.length
 end
 
@@ -57,24 +49,18 @@ def empty
 $pending.clear
 $completed.clear
 $todo.clear
-#self.save
-#self.load1
 return true
 end
 
 def show_pending(num)
-#self.save
-#self.load1
 return $pending[num-1]
 end
 
 def show_completed(num)
-#self.save
-#self.load1
 return $completed[num-1]
 end
 def save
-f = File.open("deep.txt", "w")
+f = File.open(@filename, "w")
 str =""
 str = $todo.join("\n")
 f.write(str)
